3f3be739f00676dfd10d5960e4b70af1103b2b82
caogaoli <caogaoli@baidu.com>
2020-08-27 16:58:49 +0800

    neuron termination condition.

--- neuroph-2.9/Core/src/main/java/org/neuroph/core/learning/stop/MaxIterationsStop.java
+++ neuroph-2.9/Core/src/main/java/org/neuroph/core/learning/stop/MaxIterationsStop.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010 Neuroph Project http://neuroph.sourceforge.net
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,29 +17,31 @@
 package org.neuroph.core.learning.stop;
 
 import java.io.Serializable;
+
 import org.neuroph.core.learning.IterativeLearning;
 
 /**
  * Stops learning rule if specified number of iterations has been reached
+ *
  * @author Zoran Sevarac <sevarac@gmail.com>
  */
 public class MaxIterationsStop implements StopCondition, Serializable {
 
-    private final IterativeLearning learningRule;    
-    
+    private final IterativeLearning learningRule;
+
     public MaxIterationsStop(IterativeLearning learningRule) {
         this.learningRule = learningRule;
     }
-        
+
     @Override
     public boolean isReached() {
-                
+
         if (learningRule.getCurrentIteration() >= learningRule.getMaxIterations()) {
             return true;
         }
-        
+
         return false;
     }
-    
-    
+
+
 }
--- neuroph-2.9/Core/src/main/java/org/neuroph/core/learning/stop/StopCondition.java
+++ neuroph-2.9/Core/src/main/java/org/neuroph/core/learning/stop/StopCondition.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010 Neuroph Project http://neuroph.sourceforge.net
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,16 +17,19 @@
 package org.neuroph.core.learning.stop;
 
 /**
+ * 算法终止条件.
+ * <p>
  * Interface for learning rule stop condition.
  * Classes implementing this interface should just implement one method and
  * return true if learning rule should stop
- * 
+ *
  * @author Zoran Sevarac <sevarac@gmail.com>
  */
 public interface StopCondition {
     /**
      * Returns true if learning rule should stop, false otherwise
+     *
      * @return true if learning rule should stop, false otherwise
      */
-    public boolean isReached();         
+    public boolean isReached();
 }
